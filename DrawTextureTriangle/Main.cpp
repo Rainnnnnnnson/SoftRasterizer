25925516bbbe8f128e01f99f267de55e06ec217d
Rainnnnnnnson <452881841@qq.com>
2019-06-13 23:35:18 +0800

    添加项目通用配置 添加纹理三角形

---
+++ DrawTextureTriangle/Main.cpp
@@ -0,0 +1,28 @@
+#include<windows.h>
+#include"../SoftRasterizer/Display.h"
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+					_In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+	constexpr int width = 800;
+	constexpr int height = 600;
+	Display display(hInstance, nShowCmd, width, height);
+	RGBImage fileImage = display.GetImage(L"../EZ.PNG");
+	Renderer renderer(width, height);
+
+	auto points = vector<Point3>{{0.5f, 0.4f, 0.0f}, {0.4f, -0.5f, 0.0f}, {-0.5f, 0.5f, 0.0f}};
+	auto textureCoordinates = vector<Point2>{{1.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 1.0f}};
+	//就算没有纹理也需要添加一个元素
+	auto textures = vector<RGBImage>{fileImage};
+	auto indexDatas = vector<IndexData>{{{0, 1, 2}, {0, 1, 2}, 0}};
+	auto vertexShader = [](Point3 p, Point2, const RGBImage&) {
+		return p.ToPoint4();
+	};
+	auto pixelShader = [](Point4 p, Point2 t, const RGBImage& i) {
+		return i.BilinearFiltering(t);
+	};
+	renderer.DrawTriangleByTexture<RGBImage>(points, textureCoordinates, textures, indexDatas,
+											 vertexShader, pixelShader);
+	display.Update([&]() {
+		return renderer.GenerateImage();
+	});
+	return 0;
+}
