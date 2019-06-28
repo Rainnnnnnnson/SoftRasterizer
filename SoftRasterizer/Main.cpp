189751770eb0d8d1d5e2a7c825747ca2a372c046
Rainnnnnnnson <452881841@qq.com>
2019-06-28 23:37:06 +0800

    .

--- SoftRasterizer/Main.cpp
+++ SoftRasterizer/Main.cpp
@@ -1,22 +1,32 @@
 #include<windows.h>
+#include "Rasterizer.h"
 #include "Application.h"
-
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 					_In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
-
-	PixelPointRange range{800, 600};
+	auto range = PixelPointRange{800, 600};
 	Application application(range);
-	RGBImage fileImage = application.GetImage(L"../EZ.PNG");
+	Rasterizer rasterizer(range);
+	RGBImage image = application.GetImage(L"../level0.PNG");
+	using Texture = RGBImage;
+	vector<Point3> vertexs{{-1.0f, -1.0f, 0.0f}, {-1.0f, 1.0f, 0.0f}, {1.0f, 1.0f, 0.0f}, {1.0f, -1.0f, 0.0f}};
+	vector<Point2> coordinates{{0.0f, 1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}};
+	vector<Vector3> normals{{}};
+	vector<Color> colors{{}};
+	vector<Texture> textures{std::move(image)};
+	vector<IndexData> indexs{{{0, 1, 2}, {0, 1, 2}}, {{0, 2, 3}, {0, 2, 3}}};
+	auto vertexShader = [](Point3 point, Point2&, Vector3&, Color&, const Texture&) {
+		return point.ToPoint4();
+	};
+	auto pixelShader = [](Point4, Point2 coordinate, Vector3, Color, const Texture& texture) {
+		return texture.BilinearFilter(coordinate);
+	};
 	while (application.Continue()) {
-		RGBImage image(range);
-		size_t width = fileImage.GetWidth();
-		size_t height = fileImage.GetHeight();
-		for (size_t y = 0; y < height; y++) {
-			for (size_t x = 0; x < width; x++) {
-				ImagePixelPoint point = {x, y};
-				image.SetImagePixel(point, fileImage.GetImagePixel(point));
-			}
-		}
+		rasterizer.Clear();
+		rasterizer.DrawTriangle<Texture>(vertexs, coordinates, normals, colors, textures, indexs,
+										 vertexShader, pixelShader);
+		rasterizer.DrawWireframe<Texture>(vertexs, coordinates, normals, colors, textures, indexs,
+										  vertexShader, pixelShader);
+		RGBImage image = rasterizer.GenerateRGBImage();
 		application.CopyInBuffer(std::move(image));
 	}
 	return 0;
